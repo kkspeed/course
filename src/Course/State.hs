2d3d8ac0a6b10d83d4d24f1839caf27bb2342884
bruce <leilmyxwz@gmail.com>
2014-03-22 13:05:57 -0400

    Working on state

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -26,79 +26,66 @@ import qualified Data.Set as S
 -- A `State` is a function from a state value `s` to (a produced value `a`, and a resulting state `s`).
 newtype State s a =
   State {
-    runState ::
-      s
-      -> (a, s)
+    runState :: s -> (a, s)
   }
 
 -- | Implement the `Functor` instance for `State s`.
 -- >>> runState ((+1) <$> pure 0) 0
 -- (1,0)
 instance Functor (State s) where
-  (<$>) =
-      error "todo"
+  f <$> s = State $ \x ->
+            let (v, st) = runState s x
+            in (f v, st)
 
 -- | Implement the `Apply` instance for `State s`.
 -- >>> runState (pure (+1) <*> pure 0) 0
 -- (1,0)
 instance Apply (State s) where
-  (<*>) =
-    error "todo"
+  sf <*> s = State $ \x -> let (f, _) = runState sf x
+                               (v, ss) = runState s x
+                           in (f v, ss)
+
 
 -- | Implement the `Applicative` instance for `State s`.
 -- >>> runState (pure 2) 0
 -- (2,0)
 instance Applicative (State s) where
-  pure =
-    error "todo"
+  pure x = State $ \s -> (x, s)
 
 -- | Implement the `Bind` instance for `State s`.
--- >>> runState ((const $ put 2) =<< put 1) 0
+-- >>> runState ((const $ put 2) =<< put 1) 02
 -- ((),2)
 instance Bind (State s) where
-  (=<<) =
-    error "todo"
+  f =<< s = State $ \x -> let (v, st) = runState s x
+                          in  runState (f v) st
 
 instance Monad (State s) where
 
 -- | Run the `State` seeded with `s` and retrieve the resulting state.
 --
 -- prop> \(Fun _ f) -> exec (State f) s == snd (runState (State f) s)
-exec ::
-  State s a
-  -> s
-  -> s
-exec =
-  error "todo"
+exec :: State s a -> s -> s
+exec st init = snd $ runState st init
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
 -- prop> \(Fun _ f) -> eval (State f) s == fst (runState (State f) s)
-eval ::
-  State s a
-  -> s
-  -> a
-eval =
-  error "todo"
+eval :: State s a -> s -> a
+eval st init = fst $ runState st init
 
 -- | A `State` where the state also distributes into the produced value.
 --
 -- >>> runState get 0
 -- (0,0)
-get ::
-  State s s
-get =
-  error "todo"
+get :: State s s
+get = State $ \x -> (x, x)
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
 -- >>> runState (put 1) 0
 -- ((),1)
-put ::
-  s
-  -> State s ()
-put =
-  error "todo"
+put :: s -> State s ()
+put s = State $ \_ -> ((), s)
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
@@ -114,13 +101,11 @@ put =
 --
 -- >>> let p x = (\s -> (const $ pure (x == 'i')) =<< put (1+s)) =<< get in runState (findM p $ listh ['a'..'h']) 0
 -- (Empty,8)
-findM ::
-  Monad f =>
-  (a -> f Bool)
-  -> List a
-  -> f (Optional a)
-findM =
-  error "todo"
+findM :: Monad f => (a -> f Bool) -> List a -> f (Optional a)
+findM _  Nil = pure Empty
+findM f (x:.xs) = pure x >>= f >>= \r -> if r
+                                        then pure $ Full x
+                                        else findM f xs
 
 -- | Find the first element in a `List` that repeats.
 -- It is possible that no element repeats, hence an `Optional` result.
@@ -129,12 +114,8 @@ findM =
 --
 -- prop> case firstRepeat xs of Empty -> let xs' = hlist xs in nub xs' == xs'; Full x -> length (filter (== x) xs) > 1
 -- prop> case firstRepeat xs of Empty -> True; Full x -> let (l, (rx :. rs)) = span (/= x) xs in let (l2, r2) = span (/= x) rs in let l3 = hlist (l ++ (rx :. Nil) ++ l2) in nub l3 == l3
-firstRepeat ::
-  Ord a =>
-  List a
-  -> Optional a
-firstRepeat =
-  error "todo"
+firstRepeat :: Ord a => List a -> Optional a
+firstRepeat = error "todo"
 
 -- | Remove all duplicate elements in a `List`.
 -- /Tip:/ Use `filtering` and `State` with a @Data.Set#Set@.
@@ -142,12 +123,8 @@ firstRepeat =
 -- prop> firstRepeat (distinct xs) == Empty
 --
 -- prop> distinct xs == distinct (flatMap (\x -> x :. x :. Nil) xs)
-distinct ::
-  Ord a =>
-  List a
-  -> List a
-distinct =
-  error "todo"
+distinct :: Ord a => List a -> List a
+distinct = error "todo"
 
 -- | A happy number is a positive integer, where the sum of the square of its digits eventually reaches 1 after repetition.
 -- In contrast, a sad number (not a happy number) is where the sum of the square of its digits never reaches 1
@@ -170,8 +147,5 @@ distinct =
 --
 -- >>> isHappy 44
 -- True
-isHappy ::
-  Integer
-  -> Bool
-isHappy =
-  error "todo"
+isHappy :: Integer -> Bool
+isHappy = error "todo"
